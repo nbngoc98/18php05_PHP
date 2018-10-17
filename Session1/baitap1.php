141acfcd743662c55f77fb3a9ee194982f50a376
Joony Ngọc <38630115+ngocbao152@users.noreply.github.com>
2018-10-17 16:29:58 +0700

    up

--- Session1/baitap1.php
+++ Session1/baitap1.php
@@ -1,11 +1,9 @@
 <?php 
-	// Trinh bay mang danh sach lop
 	$myClass = array(
 				'nam' => array('name' => 'Nam', 'birthday' => 1997, 'email' => 'nam@gmail.com'),
 				'nga' => array('name' => 'Nga', 'birthday' => 1998, 'email' => 'nga@gmail.com'),
 				'huong' => array('name' => 'Huong', 'birthday' => 1997, 'email' => 'huong@gmail.com'),
 			);
-	// Hien thi danh sach lop
 	function displayClass($arr) {
 		echo "<br>";
 		$i = 1;
@@ -14,11 +12,8 @@
 			$i++;
 		}
 	}
-	// Kiem tra ten va thay doi email
 	function checkNameAndChangeEmail($arr, $name, $email){
-		// Vong for nay moi, key la key khi for
 		foreach($arr as $key => $arrNew) {
-			// Neu phan tu nay co ten trung voi yeu cau thi doi email cua phan tu do
 			if ($arrNew['name'] == $name) {
 				$arr[$key]['email'] = $email;
 			}
@@ -26,9 +21,7 @@
 		return $arr;
 	}
 	function removeMemberbyName($arr, $name){
-		// Vong for nay moi, key la key khi for
 		foreach($arr as $key => $arrNew) {
-			// Neu phan tu nay co ten trung yeu cau thi loai bo phan tu do
 			if ($arrNew['name'] == $name) {
 				unset($arr[$key]);
 			}
@@ -36,16 +29,13 @@
 		return $arr;
 	}
 	function addMember($arr, $countArr,$name, $birthday, $email){
-		// Add mot phan tu moi vao mang theo du lieu
 		$arr[$countArr]['name'] = $name;
 		$arr[$countArr]['birthday'] = $birthday;
 		$arr[$countArr]['email'] = $email;
 		return $arr;
 	}
 	function checkBirthdayAndChangeEmail($arr, $birthday, $email){
-		// Vong for nay moi, key la key khi for
 		foreach($arr as $key => $arrNew) {
-			// Neu ngay sinh trung voi yeu cau thi doi email cua phan tu do
 			if ($arrNew['birthday'] == $birthday) {
 				$arr[$key]['email'] = $email;
 			}
